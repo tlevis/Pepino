1ec8f113f80c25cbb1ca5651e05039d5f45b2c6e
Tovi Levis <tlevis@gmail.com>
2014-06-13 23:20:03 +0300

    Add measurement units to IR Distance

--- user-interface/objects/IRDistance/IRDistance.js
+++ user-interface/objects/IRDistance/IRDistance.js
@@ -1,3 +1,4 @@
+var IRDistanceUnits = {"cm": "Centemeters", "inch": "Inches" }
 var IRDistanceSetupSourceEndpoint = {
 	endpoint:"Rectangle",
 	paintStyle:{ fillStyle:"#ebb600", width: 25, height: 25 },
@@ -47,10 +48,13 @@ function ObjectIRDistance (port) {
     this.Category = "Analog";
 	this.Port = (port == undefined) ? "A0" : port ;
 	this.Chain["Next"] = null;
+    this.Unit = "cm";
 	this.Position = new ObjectPosition();
 	this.SetupMode = false;
     this.Variables = [];
     this.PassedVariables;
+    this.MinValue = 10;
+    this.MaxValue = 80;
 }
 
 ObjectIRDistance.prototype.GetCode = function(passedVars) {
@@ -85,7 +89,12 @@ ObjectIRDistance.prototype.GetCode = function(passedVars) {
 	
 	var selfGlobalCode = "int " + RegisteredValuesMap[this.Variables[0]].VariableName + " = 0;";
     
-	var selfLoopCode = RegisteredValuesMap[this.Variables[0]].VariableName + " = 4800/(analogRead(" + this.Port + ") - 20);";
+    var valueDivider = "";
+    if (this.Unit == "inch") 
+    {
+        valueDivider = "/2.54";
+    }
+	var selfLoopCode = RegisteredValuesMap[this.Variables[0]].VariableName + " = (4800/(analogRead(" + this.Port + ") - 20))" + valueDivider + ";";
 
 	return  new ObjectCode(selfLoopCode + CodeLoop, CodeSetup, selfGlobalCode + CodeGlobal, CodeFunctions);
 }
@@ -118,6 +127,7 @@ ObjectIRDistance.prototype.Draw  = function(offset, working_area) {
         jsPlumb.addEndpoint(this.Id, IRDistanceTargetEndpoint, { anchor:[0.10, 0.62, 0, 0], uuid:sourceUUID });
         
         body += "<div id=\"" + this.Id + "_port_number_div\" style=\"font-family: 'ohw-font'; color: white; position: absolute; top: 25px; right: 13px; width: 25px; text-align:center\">" + this.Port + "</div>";
+        body += "<div align='center' style=\"position: absolute; top: 60px; left: 0; right:0\"><b>Units:</b><div id=\"" + this.Id + "-unit-name\" style=\"font-size: 10px\">" + IRDistanceUnits[this.Unit] + "</div></div>";
     }
     else
     {
@@ -141,7 +151,32 @@ ObjectIRDistance.prototype.UpdatePort = function UpdatePort(port) {
 
 
 ObjectIRDistance.prototype.DrawSettingsPanel  = function() {
-	var body = "";
-
+	var min = this.MinValue;
+    var max = this.MaxValue;
+    var body = "";
+    body += "<div style=\"line-height: 30px; vertical-align: middle\"><b>Units:</b> <input type=\"radio\" id=\"" + this.Id + "_output_units\" name=\"" + this.Id + "_output_units\" value=\"cm\" onclick=\"IRDistanceUnitChanged('"+this.Id+"', this.value);\" "; 
+    if (this.Unit == "cm") 
+    {
+        body += " checked "; 
+    }
+    body += "/> Centimeters ";
+    body += "&nbsp;&nbsp;&nbsp;&nbsp;<input type=\"radio\" id=\"" + this.Id + "_output_units\" name=\"" + this.Id + "_output_units\" value=\"inch\" onclick=\"IRDistanceUnitChanged('"+this.Id+"', this.value);\" ";
+    
+    if (this.Unit == "inch") 
+    {	
+        body += " checked "; 
+        min = (min / 2.54).toPrecision(2);
+        max = (max / 2.54).toPrecision(2);
+    }
+    body += "/> Inches </div>";
+    body += "<div><b>Value range:</b> " + min + " - " + max  + "</div>";
 	return body;
+}
+
+
+function IRDistanceUnitChanged(Id, value) {
+	ObjectsMap[Id].Unit = value;
+    $("#" + Id + "-unit-name").text(IRDistanceUnits[value]);    
+    ShowSettingsPanel(Id, true);
+	UpdateCode();
 }
